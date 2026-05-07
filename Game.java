9280058b946469e83b3fa4018b65320e107f0599
laura_1002948 <1002948@my.scccd.edu>
2026-05-07 00:57:52 -0700

    Main function declared

---
+++ Game.java
@@ -0,0 +1,17 @@
+import java.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Game {
+	//bool flags for movement
+	static boolean up, down, left, right;
+
+	public static void main(String[] args) {
+		JFrame window = new JFrame("Gambling Hell"); //the JFrame will create the window for the Game
+
+	}
+
+
+
+
+}
