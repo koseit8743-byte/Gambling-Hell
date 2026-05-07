eee55ebf2a0abf8655787f206d5faf0de8f59966
laura_1002948 <1002948@my.scccd.edu>
2026-05-07 09:08:38 -0700

    Defined paintComponent method

--- Game.java
+++ Game.java
@@ -8,7 +8,22 @@ public class Game {
 
 	public static void main(String[] args) {
 		JFrame window = new JFrame("Gambling Hell"); //the JFrame will create the window for the Game
+		Player jPlayer = new Player(window); //window in param to get width/height of window
 
+		JPanel visualPanel = new JPanel() {//defining cutom behavior for JPanel
+
+			
+			
+			//this is gonna get called evey repaint automatically in the game loop,
+			//anything that has to be updated visually should be added here
+			public void paintComponent(Graphics sprite) {
+				super.paintComponent(sprite); //clears old frame kinda like system("clear")
+				testText.setText("just a test");
+				jPlayer.renderPlayer(sprite);
+			}//basically overloaded swing's paintComponent method here
+		};
+
+		//in
 	}
 
 
