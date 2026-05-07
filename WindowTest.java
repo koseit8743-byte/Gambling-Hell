060e5babc914a986d47aa7943f3e01711aec15d0
laura_1002948 <1002948@my.scccd.edu>
2026-05-06 18:53:23 -0700

    Comments on code

--- WindowTest.java
+++ WindowTest.java
@@ -5,7 +5,14 @@ import java.awt.event.*;
 public class WindowTest {
 	public static void main(String[] args) {
 		JFrame window = new JFrame("Gambling Hell"); //the JFrame will create the window for the game
-		JPanel visualPanel = new JPanel() {};
 		
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//means we can close the window through top right x
+		//JPanels don't show up on the JFrame window by default
+		JPanel visualPanel = new JPanel() {}; //JPanels are gonna hold all the visual elements/sprites
+		visualPanel.setBackground(Color.GREEN);
+
+		visualPanel.setPreferredSize(new Dimension(720, 1080)); //sets the visualPanel size, it'll get added to the JFrame and the JFrame will resize to fit the panel
+		window.setVisible(true); //so the window shows up on screen
+				
 	}
 } 
