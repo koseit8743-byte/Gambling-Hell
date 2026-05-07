5779b2b98c2d15b832b6a38ce59a7ecaaf380d5f
laura_1002948 <1002948@my.scccd.edu>
2026-05-06 18:41:26 -0700

    main setup

---
+++ WindowTest.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class WindowTest {
+	public static void main(String[] args) {
+		JFrame window = new JFrame("Gambling Hell"); //the JFrame will create the window for the game
+		JPanel visualPanel = new JPanel() {};
+		
+	}
+} 
