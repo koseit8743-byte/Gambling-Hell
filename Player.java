4b92e6c377385883a5ed6dd1edcf743b3f82c60c
laura_1002948 <1002948@my.scccd.edu>
2026-05-07 09:27:34 -0700

    defined updatePos

--- Player.java
+++ Player.java
@@ -4,7 +4,18 @@ public class Player {
 	int speed = 3;
 	int x = 100;
 	int y = 100;
-	public Player() {};
-	public void updatePos() {};
+	public Player(JFrame framepanel) {
+		x = framepanel.getWidth() / 2;
+		y = framePanel.getHeight() / 2;
+	};
+	
+	//movement flags read here to start movement
+	public viod updatePos(boolean u, boolean d, boolean l, boolean r) {
+		if (u) y -= speed;
+		if (d) y += speed;
+		if (l) x += speed;
+		if (r) x -= speed;
+	}
+	
 	public void renderPlayer() {};
 }
