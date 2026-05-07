87c992d9f1fa4648964418b99dc944c2eeb4de6f
laura_1002948 <1002948@my.scccd.edu>
2026-05-07 00:59:59 -0700

    Start on Player.java

---
+++ Player.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Player {
+	int speed = 3;
+	int x = 100;
+	int y = 100;
+	public Player() {};
+	public void updatePos() {};
+	public void renderPlayer() {};
+}
