6647c863da36dcddc1f1dd4977e89f89966ba2e7
laura_1002948 <1002948@my.scccd.edu>
2026-05-06 18:59:35 -0700

    Test text added

--- WindowTest.java
+++ WindowTest.java
@@ -10,9 +10,18 @@ public class WindowTest {
 		//JPanels don't show up on the JFrame window by default
 		JPanel visualPanel = new JPanel() {}; //JPanels are gonna hold all the visual elements/sprites
 		visualPanel.setBackground(Color.GREEN);
+		
+		JLabel testText = new JLabel("just a test");
+		visualPanel.add(testText);
 
-		visualPanel.setPreferredSize(new Dimension(720, 1080)); //sets the visualPanel size, it'll get added to the JFrame and the JFrame will resize to fit the panel
+		visualPanel.setPreferredSize(new Dimension(1080, 720)); //sets the visualPanel size, it'll get added to the JFrame and the JFrame will resize to fit the panel
+		
+
+		window.add(visualPanel); //adds the panel to the window
+		window.pack(); //resizes window to fit the panel
+		
+
 		window.setVisible(true); //so the window shows up on screen
-				
+					
 	}
 } 
