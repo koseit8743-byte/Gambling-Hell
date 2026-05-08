fcbd87a617ad08136e85585d5426e0798b82f37b
koseit9001 <koseit8743@gmail.com>
2026-05-07 21:02:44 -0700

    client stuff  for sever rough draft

---
+++ Client.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.net.*;
+import java.util.scanner;
+
+public class Client {
+	private static final int PORT = 49822;
+
+	private static final String SERVER_ADDRESS = "localhost";
+
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Enter your name: ");
+		String name = scanner.nextLine();
+
+		System.out.println("Connecting to " + " on port " +PORT + "...");
+
+		try(Socket socket = new Socket(SERVER_ADDRESS, PORT)){
+
+			PringWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+			System.out.println("Connected to server!");
+
+			Thread recieveThread = new Thread(()-> {
+				try{
+					String message;
+					while ((message = in.readLine()) != null){
+						System.outprintln(message);
+					}
+				}catch(IOException e){
+					System.out.println("Disconnected from server");
+				}
+			});
+
+			recieveThread.start();
+
+			System.out.println("Type messages ( type quit to exit):");
+			String input;
+			while(!(input = scanner.nextLine()).equalsIgnoreCase("quit)){
+				out.println(name + ": " + input);
+			}
+
+			System.out.println("Disconnecting....");
+
+		}catch(IOException e) {
+
+			System.out.println("Could not connect to server at" + SERVER_ADDRESS + ":" + PORT);
+		}
+
+		scanner.close();
+	}
+
+}
