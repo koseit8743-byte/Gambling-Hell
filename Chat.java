a164b7bf40264bee8088c874e98e2f82b3f17f9d
laura_1002948 <1002948@my.scccd.edu>
2026-05-08 10:33:32 -0700

    defining Constructor

--- Chat.java
+++ Chat.java
@@ -6,6 +6,17 @@ public class Chat {
 	JFrame chatWindow;
 	JPanel chatPanel;
 
+
 	JTextArea chatArea;
 	JTextField inputField;
+	//JLabel message;
+	
+	public Chat() {
+		chatWindow = new JFrame("Gambling Hell Chat");
+		chatPanel = new JPanel();
+		
+		chatPanel.setLayout(new BorderLayout());
+		chatArea = new JTextArea();
+		chatArea.setEditable(false);
+	}
 }
