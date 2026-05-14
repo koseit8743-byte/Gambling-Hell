9f658967ce262505ec77e5c96d5745842915e6c3
laura_1002948 <1002948@my.scccd.edu>
2026-05-13 20:21:26 -0700

    Enemy sprite added

--- Enemy.java
+++ Enemy.java
@@ -1,14 +1,19 @@
 import java.awt.Rectangle;
 import java.awt.Graphics;
 import java.awt.Color;
+import javax.swing.ImageIcon;
+import java.awt.Image;
 
 public class Enemy{
 	int x,y,width=30,height=30, health=3, speed=4;
 	boolean exists=true;
+	
+	Image eSprite;
 
 	public Enemy(int initial_x, int initial_y){
 		x=initial_x;
 		y=initial_y;
+		eSprite = new ImageIcon("Sprites/devil.gif").getImage();
 	}
 
 	public void update_enemy(){
@@ -28,7 +33,8 @@ public class Enemy{
 	}
 
 	public void renderEnemy(Graphics enemysprite) {
-		enemysprite.setColor(Color.RED);
-		enemysprite.fillRect(x,y,width,height);
+		//enemysprite.setColor(Color.RED);
+		//enemysprite.fillRect(x,y,width,height);
+		enemysprite.drawImage(eSprite, x,y, width,height,null);
 	}
 }
